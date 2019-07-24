5e5db41da21efbfa8e4f1ef89cbf25c5d7f989eb
unknown <buziyborni@gmail.com>
2019-07-24 20:07:39 +0100

    minor change

---
+++ README.md
@@ -0,0 +1,1 @@
+# express static server that let you load html, css, javascript, image etc. without you setting up each route in nodejs
--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 port = process.env.PORT || 3000;
-app.listen(port, console.log('listing on port ' + port));
+app.listen(port, console.log(`listing on port ${port}`));
